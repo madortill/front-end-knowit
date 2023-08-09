9c075731d502fa84740ec886571145887cb05548
Hadar Yeheskiel <hadaryehskiel@Madors-iMac.mshome.net>
2023-08-09 14:13:00 +0300

    main page 2

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import NavBar from "./components/NavBar/NavBar.component";
 import './App.css';
 
 function App() {
+  const event_date = new Date('2023-11-08T08:00:00');
+  const dateTimeAfterNumDays = event_date; 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<NavBar />}>
-          <Route path="home" element={<MainPage />}></Route>
+          <Route path="home" element={<MainPage targetDate={dateTimeAfterNumDays}/>}></Route>
         </Route>
       </Routes>
-
     </div>
   );
 }
--- src/components/MainPage/MainPage.component.jsx
+++ src/components/MainPage/MainPage.component.jsx
@@ -1,32 +1,45 @@
-import './MainPage.styles.css'
+import './MainPage.styles.css';
+import CountdownTimer from '../countdown/CountdownTimer.component';
+import Slider from '../slider/slider.component';
 
-const MainPage = () => {
+const MainPage = (props) => {
+
+    const DATA = [
+        'מנהלים ואנשי פיתוח למידה, קריית ההדרכה גאה להזמינכם לכנס ייחודי ומיוחד במינו בנושא חדשנות בלמידה.  הכנס מיועד למפתחי הדרכה בכירים, מנהלי הדרכה בארגון, אנשי חינוך, הוראה ולמידה בצה"ל, כוחות הבטחון השונים, אקדמיה, גופי חינוך ולמידה בארגונים השונים.',
+        'מדור טי״ל של ענף הדרכה בקריית ההדרכה בנגב מתמחה ביצירת תוצרים דיגיטליים וחדשניים להדרכה. תוצריו מגיעים להדרכות השונות ולקורסים בקריית ההדרכה ובצה״ל ככלל. גם השנה אנו גאים לארח אתכם בכנס חדשני ומיוחד במינו.'
+    ]
     return (
-      <div className="main-page">
-        <span className='section1 section'>
-            <div className='white-section1'>
-                <h1 className='main-header'>למידה מעולם לא הייתה חדשנית יותר.</h1>
-                <p className='small-text'>כנס חדשנות בלמידה 2023</p>
-                <button className='sign-btn pointer'>להרשמה</button>
-            </div>
-            <div className='black-section1'></div>
-        </span>
-        <span className='section2 section'>
-            <div className='white-section2'>
-                <div>
-                    <div className='slider'></div>
-                    <div>
-                        <h1 className='header'>KNOW IT?</h1>
-                        <p></p>
-                    </div>
+        <div className="main-page">
+            <span className='section1 section'>
+                <div className='white-section1'>
+                    <h1 className='main-header'>למידה מעולם לא הייתה חדשנית יותר.</h1>
+                    <p className='small-text'>כנס חדשנות בלמידה 2023</p>
+                    <button className='sign-btn pointer'>להרשמה</button>
                 </div>
-                
+                <div className='black-section1'></div>
+            </span>
+            <span className='section2 section'>
+                <div className='white-section2'>
+                    <div className='text-pic-section text-pic-section1'>
+                        <Slider className='slider'></Slider>
+                        <div className='text-section text-section1'>
+                            <h1 className='header'>KNOW IT?</h1>
+                            <p className="main-page-text">{DATA[0]}</p>
+                        </div>
+                    </div>
+                    <div className='text-pic-section text-pic-section2'>
+                        <div className='logo-till'></div>
+                        <div className='text-section text-section2'>
+                            <h1 className='header'>מדור טי״ל</h1>
+                            <p className="main-page-text">{DATA[1]}</p>
+                        </div>
+                    </div>
+                    <CountdownTimer targetDate={props.targetDate} />
+                </div>
+            </span>
 
         </div>
-        </span>
-        
-      </div>
     );
 }
-  
+
 export default MainPage;
