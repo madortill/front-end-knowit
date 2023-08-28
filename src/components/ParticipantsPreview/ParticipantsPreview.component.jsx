183554befd3f8f3730141cb8c4d0dfe8e45eadd6
madortill <mador.till@gmail.com>
2023-08-28 14:28:13 +0300

    message

--- src/components/ParticipantsPreview/ParticipantsPreview.component.jsx
+++ src/components/ParticipantsPreview/ParticipantsPreview.component.jsx
@@ -6,12 +6,25 @@ import person2 from '../../media/images/people/gila_jpg.webp';
 import person3 from '../../media/images/people/yuval.jpeg';
 import person4 from '../../media/images/people/aviramdaniel.png';
 import person5 from '../../media/images/people/snir.jpg';
+import person6 from '../../media/images/people/adi.jpg';
+import person7 from '../../media/images/people/shirlee.jpg';
+import person8 from '../../media/images/people/Elad_Darmon.jpeg';
 import fillerman from '../../media/images/man.svg';
 import fillerwoman from '../../media/images/woman.svg';
 
 const ParticipantsPreview = () => {
     const navigate = useNavigate();
     const PEOPLE = [
+        {
+            "src": person7,
+            "name": "",
+            "title": ""
+        },
+        {
+            "src": person8,
+            "name": "",
+            "title": ""
+        },
         {
             "src": person3,
             "name": "",
@@ -38,15 +51,11 @@ const ParticipantsPreview = () => {
             "title": ""
         },
         {
-            "src": fillerman,
+            "src": person6,
             "name": "",
             "title": ""
         },
-        {
-            "src": fillerwoman,
-            "name": "",
-            "title": ""
-        }
+        
     ]
     return (
         <>
